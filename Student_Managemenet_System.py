0025ead76673cc98e9cc0c81fd2bfc566e80dc10
Nafis Khan <nafiskhan59786@gmail.com>
2026-08-04 21:12:27 +0530

    added student input and storage

--- Student_Managemenet_System.py
+++ Student_Managemenet_System.py
@@ -1,1 +1,15 @@
-name=input("Enter Student name")
+students=[]
+while True:
+
+    name=input("Enter Student Name     :- ")
+    roll=input("Enter Student Roll No  :- ")
+    marks=input("Enter Student Marks    :-")
+    again=input('"Press "ENTER" for new entry "n" for Exit"')
+    student={"Name":name,"Roll":roll,"Marks":marks}
+    students.append(student)
+    
+    if again=="n":
+        break
+
+print(students)
+
