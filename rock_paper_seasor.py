70d8530d436620f2b89fa06cc7997a22ff117a96
Nafis Khan <nafiskhan59786@gmail.com>
2026-08-06 20:07:56 +0530

    upgrade code and fix nugs

--- rock_paper_seasor.py
+++ rock_paper_seasor.py
@@ -2,37 +2,38 @@ import random
 print("=======  wecome to the rock paper seasor game ======")
 again=True
 while again==True :
-    code=int(input("enter 1 for rock 2 for paper and 3 for scisors"))
-    if code==1:
-        user_chice="rock"
-    elif code==2:
-        user_chice="paper"
-    elif code==3:
-        user_chice="scissors"
-    else:
-        print("invalid input")
+    while True:
+        code=int(input("enter 1 for rock 2 for paper and 3 for scisors"))
+        if code==1:
+            user_chice="rock"
+            break
+        elif code==2:
+            user_chice="paper"
+            break
+        elif code==3:
+            user_chice="scissors"
+            break
+        else:
+            print("invalid input")
+
     com_choice=random.choice(["rock","paper","scissors"])
     print("Your choice is      :- ",user_chice)
     print("Computer choice is  :-",com_choice)
 
-    if (user_chice=="rock" and com_choice=="paper")or(user_chice=="paper" and com_choice=="rock")or(user_chice=="scissors"and com_choice=="paper"):
-        winner=1
+    if (user_chice=="rock" and com_choice=="scissors")or(user_chice=="paper" and com_choice=="rock")or(user_chice=="scissors"and com_choice=="paper"):
+        winner="you"
     else:
-        winner=2
+        winner="computer"
 
 
-
+    print ("===RESULT OF GAME=====")
     if com_choice==user_chice:
-        print ("===RESULT OF GAME=====")
         print ("==DRAW==")
-    elif winner==1:
-        print ("===RESULT OF GAME=====")
-        print("You won the game")
-    elif winner==2:
-        print ("===RESULT OF GAME=====")
-        print("Computer won the game")
-    aga=input("press 'ENTER' for play again ('n') for 'EXIT' ")
-    if aga=="n":
+    print("====",winner," won the game =====")
+    print("====",winner," won the game =====")
+
+    play_again=input("press 'ENTER' for play again ('n') for 'EXIT' ")
+    if play_again=="n":
         break
 print("==== thanks for playing this game")
     
